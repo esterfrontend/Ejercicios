c7c74b96e6ba385507f98c8ca6fb7309a21eff2f
Ester Martínez <ester.frontend@gmail.com>
2024-02-07 15:36:56 +0100

    Reservas v1

--- 10-Router Express/README.md
+++ 10-Router Express/README.md
@@ -1,6 +1,43 @@
 # RESERVAS DE HOTEL
 
 ## Registro de nuevo usuario
-Nombre, apellido y DNI (sin letra) son campos obligatorios que deben pasarse en el body.
-La petición se realiza mediante el método POST en la url "/registro".
-Si la petición es correcta, revisa si el DNI ya existe en la BBDD, si no existe guardará el nuevo cliente en la BBDD. En caso de que ya exista, devolverá el error.
+Campos necesarios:
+- Nombre
+- Apellido
+- DNI (sin letra)
+
+Todos los campos son obligatorios y deben pasarse en el body. Ejemplo:
+
+```json
+{
+    "nombre": "María",
+    "apellido": "García",
+    "dni": 79243574
+}
+````
+
+La petición se realiza mediante el **método POST** en la **url "/registro"**.
+
+Se comprueba que el cliente no existe previamente en la BBDD, si no existe guardará el nuevo cliente. En caso de que ya exista, devolverá error indicando la razón.
+
+
+## Editar cliente
+Podrán editarse tanto nombre como apellidos, el DNI no es editable.
+
+La petición se realiza mediante el **método PUT** en la **url "/editar-cliente"**.
+
+Se le debe pasar en el body el DNI del cliente al que queremos modificar y los datos a modificar. Ejemplo:
+
+```json
+{
+    "nombre": "Juan",
+    "apellido": "López",
+    "dni": 79243574
+}
+````
+
+Se comprueba que el cliente existe en la BBDD, si existe hará las modificaciones. En caso de que no exista, devolverá error indicando la razón.
+
+
+## Check in
+
--- 10-Router Express/index.js
+++ 10-Router Express/index.js
@@ -19,9 +19,9 @@ app.use(express.json())
 // Registro de nuevo cliente
 app.post('/registro', async (req, res) => {
     try {
-        const clienteAntiguo = await Cliente.find({dni: req.body.dni})
+        const clienteAntiguo = await Cliente.findOne({dni: req.body.dni})
         
-        if(clienteAntiguo.length > 0) {
+        if(clienteAntiguo) {
             res.send({mensaje: 'Este cliente ya está registrado', clienteAntiguo})
         } else {
             const clienteNuevo = await Cliente.create(req.body)
@@ -41,19 +41,23 @@ app.put('/editar-cliente', async (req, res) => {
             req.body, 
             {new: true}
         )
-        res.send({mensaje: 'Cliente modificado', clienteEditado})
+        if(clienteEditado) {
+            res.send({mensaje: 'Cliente modificado', clienteEditado})
+        } else {
+            res.send({mensaje: 'Este cliente no está registrado'})
+        }
     } catch (error) {
         res.send({mensaje: 'El cliente no ha podido modificarse', error})
     }
 })
 
 
-// Reservar habitación
-app.put('/reservar', async (req, res) => {
+// Checkin
+app.put('/checkin', async (req, res) => {
     try {
         let {cliente, habitacion} = req.body
-        const clienteReserva = await Cliente.find({dni: cliente})
-        const habitacionReservada = await Habitacion.find({$and: [
+        const clienteReserva = await Cliente.findOne({dni: cliente})
+        const habitacionSolicitada = await Habitacion.findOne({$and: [
             {number: habitacion}, {reservada: false}
         ]})
         
@@ -61,24 +65,64 @@ app.put('/reservar', async (req, res) => {
         if(clienteReserva.length === 0) {
             // Cliente no registrado
             res.send({mensaje: `El cliente ${cliente} no está registrado`})
-        } else if (habitacionReservada.length === 0) {
+        } else if (habitacionSolicitada.length === 0) {
             // Habitación no disponible
             res.send({mensaje: `La habitación ${habitacion} no está disponible`})
         } else {
-            // Todo bien
-            const reserva = await Habitacion.findOneAndUpdate(
+            // Se crea la reserva
+            let reserva = await Reserva.create({
+                    cliente: clienteReserva._id,
+                    habitacion: habitacionSolicitada._id,
+                    fechaCheckIn: new Date,
+                    fechaCheckOut: new Date
+                })
+            
+            // La habitación se marca como reservada
+            const habitacionReservada = await Habitacion.findOneAndUpdate(
                 {number: habitacion}, 
                 {$set: {reservada: true}}, 
                 {new: true}
             )
-            res.send({mensaje: `Habitación ${habitacion} reservada por ${cliente}`, reserva})
+            res.send({mensaje: `Habitación ${habitacion} reservada por ${cliente}`, reserva, habitacionReservada})
         }
-        res.end()
     } catch (error) {
         res.send({mensaje: 'Error en la reserva', error})
     }
 })
 
+// Checkin
+app.put('/checkout', async (req, res) => {
+    try {
+        let {cliente} = req.body
+        let clienteReserva = await Cliente.findOne({dni: cliente})
+        let reserva = await Reserva.findOne({cliente: clienteReserva._id})  
+             
+        if(reserva.length === 0) {
+            // Cliente sin reserva
+            res.send({mensaje: `El cliente ${cliente} no tiene ninguna reserva`})
+        } else {
+            // Cambiar fecha de chekout  
+            const reservaCheckOut = await Reserva.findOneAndUpdate(
+                {cliente: clienteReserva._id}, 
+                {$set: {fechaCheckOut: new Date}},
+                {new: true}
+            )
+                
+            // La habitación vuelve a estar disponible
+            const habitacionID = reserva.habitacion.toString()
+            
+            const habitacionReservada = await Habitacion.findByIdAndUpdate(
+                habitacionID, 
+                {$set: {reservada: false}},
+                {new: true}
+            )
+
+            res.send({mensaje: `Checkout realizado`, checkout: reservaCheckOut, habitacion: habitacionReservada})
+        }
+    } catch (error) {
+        res.send({mensaje: 'Error en la reserva', error})
+    }
+})
 
 
 app.listen(PORT, (e) => {
